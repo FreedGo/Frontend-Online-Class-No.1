cca8b2c7202f01575a94058412ac34e198e94180
Freed <flyxz@126.com>
2017-03-01 09:57:46 +0800

    新的简繁体转换

--- 练习/DOM/test.js
+++ 练习/DOM/test.js
@@ -1,9 +1,13 @@
 /**
  * Created by Freed on 2017/2/13.
  */
-
-function Fn() {
-    return Fn
-};
-fn = new Fn();
-console.log(fn instanceof Fn);
+var str = 1487572974;
+		str = str * 1000;//秒转为毫秒
+var date = new Date(str);
+var hour = date.getHours();
+var mins = date.getMinutes();
+var second = date.getSeconds();
+console.log(date)
+console.log(hour)
+console.log(mins)
+console.log(second)
